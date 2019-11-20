23844af55200d3a8735734200b845df9f6314b44
jhliebo23 <57269484+jhliebo23@users.noreply.github.com>
2019-11-19 20:55:40 -0600

    commit 01
    I made some draft secnery and added in some code. All should be updated eventually.

---
+++ Leaps Of Faith/Assets/PlayerScr.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScr : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveDirV = Input.GetAxis("Vertical");
+        float moveDirH = Input.GetAxis("Horizontal");
+
+        float turnDirH = Input.GetAxis("Mouse X");
+
+
+        transform.Translate( 10 * moveDirH * Time.deltaTime, 0, 10 * Time.deltaTime * moveDirV);
+        transform.Translate(10 * turnDirH * Time.deltaTime,0,0);
+
+       if (Input.GetKeyDown("space"))
+        {
+
+            transform.Translate(0,2,0);
+
+        }
+
+
+
+
+
+
+
+        } 
+    }
+
