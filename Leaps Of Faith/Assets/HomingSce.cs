642d75a026d881498de01aab5bb170655cc2d6c9
jhliebo23 <57269484+jhliebo23@users.noreply.github.com>
2019-11-25 10:16:58 -0600

    commit 6.1 Jack

---
+++ Leaps Of Faith/Assets/HomingSce.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomingSce : MonoBehaviour
+{
+
+    public transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, Time.deltaTime * 1);
+        Transform.LookAt(target);
+    }
+}
+}
